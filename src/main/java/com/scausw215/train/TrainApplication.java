1ace3e0297821de86ba6f4cca2747f9e0ac2c6b4
SEN <2110901906@qq.com>
2023-03-24 11:02:59 +0800

    项目构建

---
+++ src/main/java/com/scausw215/train/TrainApplication.java
@@ -0,0 +1,18 @@
+package com.scausw215.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sen
+ * @date 2023/3/24 11:02
+ * @description 项目启动类
+ */
+@SpringBootApplication
+public class TrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainApplication.class, args);
+    }
+
+}
