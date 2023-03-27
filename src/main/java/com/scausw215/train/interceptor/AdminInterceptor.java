cb90a2bb66b9cb484d8147126d6d90529e02b06e
SEN <2110901906@qq.com>
2023-03-27 14:14:29 +0800

    1. 增加了NO_PERMISSION错误码 2. 权限拦截器增加了返回错误信息和log日志

--- src/main/java/com/scausw215/train/interceptor/AdminInterceptor.java
+++ src/main/java/com/scausw215/train/interceptor/AdminInterceptor.java
@@ -1,16 +1,23 @@
 package com.scausw215.train.interceptor;
 
+import com.alibaba.fastjson.JSON;
+import com.scausw215.train.common.ErrorCode;
 import com.scausw215.train.constant.UserInfoConstant;
 import com.scausw215.train.entity.DO.PassengerDO;
 import com.scausw215.train.entity.DO.UserInfoDO;
+import com.scausw215.train.utils.ResultUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.PrintWriter;
 
 /**
  * @description 管理员拦截器
  * @author sensnow
  */
+@Slf4j
 public class AdminInterceptor implements HandlerInterceptor {
 
     @Override
@@ -18,6 +25,17 @@ public class AdminInterceptor implements HandlerInterceptor {
         // 获取用户信息
         UserInfoDO userInfoDO = (UserInfoDO) request.getSession().getAttribute(UserInfoConstant.USER_INFO_STATE);
         // 判断用户是否为管理员
-        return userInfoDO.getUserAuthority() == 1;
+        if(userInfoDO.getUserAuthority() != 1) {
+            log.error("AdminInterceptor.preHandle: {}不是管理员", userInfoDO);
+            response.setStatus(401);
+            response.setContentType("application/json;charset=UTF-8");
+            PrintWriter writer = response.getWriter();
+            writer.write(JSON.toJSONString(ResultUtils.error(ErrorCode.NOT_LOGIN, "未登录")));
+            writer.flush();
+            writer.close();
+            return false;
+        }
+        log.info("AdminInterceptor.preHandle: {}是管理员 ", userInfoDO);
+        return true;
     }
 }
